1e52b067f3da9258d996c776b6a937537a3e19b0
hyGuo00 <72924386+hyGuo00@users.noreply.github.com>
2023-04-11 10:28:36 +0800

    修改页面样式

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -176,9 +176,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>Question answering assistant</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            Let's start with questions.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
